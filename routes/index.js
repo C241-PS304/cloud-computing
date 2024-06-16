1b080c38a16df6d092cd44095b8df58705c00704
fadia <99716960+FadiaNurShafitri@users.noreply.github.com>
2024-06-16 14:35:44 +0700

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const userRoutes = require("./userRoutes");
-
-const router = Router();
-
-// User
-router.use("/user", userRoutes);
-
-module.exports = router;
